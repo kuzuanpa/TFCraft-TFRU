f2333bbb703efa75ca9ec723e37a2ae6ac4b6377
kuzuanpa <misaka16384@outlook.com>
2024-06-02 21:58:52 +0800

    add deep search for ProPick

--- src/Common/com/bioxx/tfc/Items/Tools/ItemProPick.java
+++ src/Common/com/bioxx/tfc/Items/Tools/ItemProPick.java
@@ -2,10 +2,10 @@ package com.bioxx.tfc.Items.Tools;
 
 import java.util.*;
 
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+import com.bioxx.tfc.Blocks.BlockTerra;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -65,144 +65,142 @@ public class ItemProPick extends ItemTerra
 	}
 
 	@Override
-	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ){
+		return searchOre(itemStack,player,world,x,y,z, false);
+	}
+	@Override
+	public boolean onBlockDestroyed(ItemStack itemStack, World world, Block block, int x, int y, int z, EntityLivingBase entity) {
+		if(!(entity instanceof EntityPlayer)|| !(block instanceof BlockTerra))return false;
+		else return searchOre(itemStack,(EntityPlayer) entity,world,x,y-50,z, true);
+	}
+	public boolean searchOre(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, boolean isDeepSearch)
 	{
+		if (world.isRemote)return true;
 		Block block = world.getBlock(x, y, z);
-		if (!world.isRemote)
-		{
-			// Negated the old condition and exiting the method here instead.
-			if (block == TFCBlocks.toolRack)
-				return true;
+		// Negated the old condition and exiting the method here instead.
+		if (block == TFCBlocks.toolRack)
+			return true;
 
-			// Getting the metadata only when we actually need it.
-			int meta = world.getBlockMetadata(x, y, z);
+		// Getting the metadata only when we actually need it.
+		int meta = world.getBlockMetadata(x, y, z);
 
-			SkillRank rank = TFC_Core.getSkillStats(player).getSkillRank(Global.SKILL_PROSPECTING);
+		SkillRank rank = TFC_Core.getSkillStats(player).getSkillRank(Global.SKILL_PROSPECTING);
 
+		if(rank.equals(SkillRank.Novice)&&isDeepSearch){
+			tellCannotDeepSearch(player);
+			return true;
+		}
 			// If an ore block is targeted directly, it'll tell you what it is.
-			if ((block == TFCBlocks.ore ||	block == TFCBlocks.ore2 || block == TFCBlocks.ore3) && 
-					world.getTileEntity(x, y, z) instanceof TEOre)
-			{
-				TEOre te = (TEOre) world.getTileEntity(x, y, z);
-				if (block == TFCBlocks.ore && rank == SkillRank.Master)
-					meta = ((BlockOre) block).getOreGrade(te, meta);
-				if (block == TFCBlocks.ore2) meta = meta + Global.ORE_METAL.length;
-				if (block == TFCBlocks.ore3) meta = meta + Global.ORE_METAL.length + Global.ORE_MINERAL.length;
-				tellResult(player, new ItemStack(TFCItems.oreChunk, 1, meta));
-				return true;
-			}
-			else if (!TFC_Core.isGround(block)) // Exclude ground blocks to help with performance
-			{
-				Iterator iter = WorldGenOre.oreList.values().iterator();
-				while (iter.hasNext())
-				{
-					OreSpawnData osd = (OreSpawnData) iter.next();
-					if (osd != null && block == osd.block)
-					{
-						tellResult(player, new ItemStack(block));
-						return true;
-					}
+		if ((block == TFCBlocks.ore ||	block == TFCBlocks.ore2 || block == TFCBlocks.ore3) &&
+				world.getTileEntity(x, y, z) instanceof TEOre)
+		{
+			TEOre te = (TEOre) world.getTileEntity(x, y, z);
+			if (block == TFCBlocks.ore && rank == SkillRank.Master)
+				meta = ((BlockOre) block).getOreGrade(te, meta);
+			if (block == TFCBlocks.ore2) meta = meta + Global.ORE_METAL.length;
+			if (block == TFCBlocks.ore3) meta = meta + Global.ORE_METAL.length + Global.ORE_MINERAL.length;
+			tellResult(player, new ItemStack(TFCItems.oreChunk, 1, meta));
+			return true;
+		}
+		else if (!TFC_Core.isGround(block)) // Exclude ground blocks to help with performance
+		{
+			for (OreSpawnData osd : WorldGenOre.oreList.values()) {
+				if (osd != null && block == osd.block) {
+					tellResult(player, new ItemStack(block));
+					return true;
 				}
 			}
+		}
 
-			random = new Random(x * z + y);
-			int chance = 60 + ((rank.ordinal()+1)*10);
+		random = new Random(x * z + y);
+		int chance = 60 + ((rank.ordinal()+1)*10);
 
-			results.clear();
-			// If random(100) is less than 60, it used to succeed. we don't need to
-			// gather the blocks in a 25x25 area if it doesn't.
-			if (random.nextInt(100) >= chance && rank != SkillRank.Master)
+		results.clear();
+		// If random(100) is less than 60, it used to succeed. we don't need to
+		// gather the blocks in a 25x25 area if it doesn't.
+		if (random.nextInt(100) >= chance && rank != SkillRank.Master)
+		{
+			tellNothingFound(player);
+			return true;
+		}
+
+		results.clear();
+
+		// Check all blocks in the 25x25 area, centered on the targeted block.
+		for (int i = -12; i < 12; i++)
+		{
+			for (int j = -12; j < 12; j++)
 			{
-				tellNothingFound(player);
-				return true;
-			}
-
-			results.clear();
-			
-			// Check all blocks in the 25x25 area, centered on the targeted block.
-			for (int i = -12; i < 12; i++)
-			{
-				for (int j = -12; j < 12; j++)
+				for(int k = -12; k < 12; k++)
 				{
-					for(int k = -12; k < 12; k++)
-					{
-						int blockX = x + i;
-						int blockY = y + j;
-						int blockZ = z + k;
+					int blockX = x + i;
+					int blockY = y + j;
+					int blockZ = z + k;
 
-						block = world.getBlock(blockX, blockY, blockZ);
-						meta = world.getBlockMetadata(blockX, blockY, blockZ);
-						ItemStack ore = null;
-						if (block == TFCBlocks.ore && world.getTileEntity(blockX, blockY, blockZ) instanceof TEOre)
-						{
-							TEOre te = (TEOre) world.getTileEntity(blockX, blockY, blockZ);
-							if (rank == SkillRank.Master)
-								ore = new ItemStack(TFCItems.oreChunk, 1, ((BlockOre) block).getOreGrade(te, meta));
-							else
-								ore = new ItemStack(TFCItems.oreChunk, 1, meta);
+					block = world.getBlock(blockX, blockY, blockZ);
+					meta = world.getBlockMetadata(blockX, blockY, blockZ);
+					ItemStack ore = null;
+					if (block == TFCBlocks.ore && world.getTileEntity(blockX, blockY, blockZ) instanceof TEOre)
+					{
+						TEOre te = (TEOre) world.getTileEntity(blockX, blockY, blockZ);
+						if (rank == SkillRank.Master)
+							ore = new ItemStack(TFCItems.oreChunk, 1, ((BlockOre) block).getOreGrade(te, meta));
+						else
+							ore = new ItemStack(TFCItems.oreChunk, 1, meta);
+					}
+					else if (block == TFCBlocks.ore2)
+						ore = new ItemStack(TFCItems.oreChunk, 1, meta + Global.ORE_METAL.length);
+					else if (block == TFCBlocks.ore3)
+						ore = new ItemStack(TFCItems.oreChunk, 1, meta + Global.ORE_METAL.length + Global.ORE_MINERAL.length);
+					else if (!TFC_Core.isGround(block)) // Exclude ground blocks to help with performance
+					{
+						for (OreSpawnData osd : WorldGenOre.oreList.values()) if (osd != null && block == osd.block) {
+							ore = new ItemStack(block);
+							break;
 						}
-						else if (block == TFCBlocks.ore2)
-							ore = new ItemStack(TFCItems.oreChunk, 1, meta + Global.ORE_METAL.length);
-						else if (block == TFCBlocks.ore3)
-							ore = new ItemStack(TFCItems.oreChunk, 1, meta + Global.ORE_METAL.length + Global.ORE_MINERAL.length);
-						else if (!TFC_Core.isGround(block)) // Exclude ground blocks to help with performance
-						{
-							Iterator iter = WorldGenOre.oreList.values().iterator();
-							while (iter.hasNext())
-							{
-								OreSpawnData osd = (OreSpawnData) iter.next();
-								if (osd != null && block == osd.block)
-								{
-									ore = new ItemStack(block);
-									break;
-								}
+
+					}
+					else
+						continue;
+
+					if (ore != null)
+					{
+						String oreName = ore.getDisplayName();
+
+						if (results.containsKey(oreName)){
+							results.get(oreName).count++;
+							if( rank == SkillRank.Master){
+								results.get(oreName).oreExistsArea[blockY-y<-2?0:blockY-y>2?2:1][blockX-x<-2?0:blockX-x>2?2:1][blockZ-z<-2?0:blockZ-z>2?2:1]=true;
+							} else {
+								results.get(oreName).averageCalculateX += blockX;
+								results.get(oreName).averageCalculateY += blockY;
+								results.get(oreName).averageCalculateZ += blockZ;
 							}
 						}
 						else
-							continue;
+							results.put(oreName, new ProspectResult(ore, 1,blockX,blockY,blockZ));
 
-						if (ore != null)
-						{
-							String oreName = ore.getDisplayName();
-
-							if (results.containsKey(oreName)){
-								results.get(oreName).count++;
-								if( rank == SkillRank.Master){
-									results.get(oreName).oreExistsArea[blockY-y<-2?0:blockY-y>2?2:1][blockX-x<-2?0:blockX-x>2?2:1][blockZ-z<-2?0:blockZ-z>2?2:1]=true;
-								} else {
-									results.get(oreName).averageCalculateX += blockX;
-									results.get(oreName).averageCalculateY += blockY;
-									results.get(oreName).averageCalculateZ += blockZ;
-								}
-							}
-							else
-								results.put(oreName, new ProspectResult(ore, 1,blockX,blockY,blockZ));
-
-							ore = null;
-							oreName = null;
-						}
+						ore = null;
+						oreName = null;
 					}
 				}
 			}
-			
-			// Tell the player what was found.
-			if (results.isEmpty()) {
-				tellNothingFound(player);
-			} else {
-				tellResult(player);
-			}
-
-			results.clear();
-			random = null;
-
-			// Damage the item on prospecting use.
-			itemStack.damageItem(1, player);
-			if (itemStack.getItemDamage() >= itemStack.getMaxDamage())
-				player.destroyCurrentEquippedItem();
 		}
 
+		// Tell the player what was found.
+		if (results.isEmpty()) tellNothingFound(player);
+		else tellResult(player, isDeepSearch);
+
+
+		results.clear();
+		random = null;
+
+		// Damage the item on prospecting use.
+		itemStack.damageItem(isDeepSearch?4:1, player);
+		if (itemStack.getItemDamage() >= itemStack.getMaxDamage()) player.destroyCurrentEquippedItem();
 		return true;
 	}
+
 
 	/*
 	 * Tells the player nothing was found.
@@ -210,6 +208,10 @@ public class ItemProPick extends ItemTerra
 	private void tellNothingFound(EntityPlayer player)
 	{
 		TFC_Core.sendInfoMessage(player, new ChatComponentTranslation("gui.ProPick.FoundNothing"));
+	}
+	private void tellCannotDeepSearch(EntityPlayer player)
+	{
+		TFC_Core.sendInfoMessage(player, new ChatComponentTranslation("gui.ProPick.cannotDeepSearch"));
 	}
 
 	/*
@@ -228,12 +230,11 @@ public class ItemProPick extends ItemTerra
 	/*
 	 * Tells the player what ore has been found, randomly picked off the HashMap.
 	 */
-	private void tellResult(EntityPlayer player)
+	private void tellResult(EntityPlayer player, boolean isDeepSearch)
 	{
 
-		TFC_Core.getSkillStats(player).increaseSkill(Global.SKILL_PROSPECTING, 1);
+		TFC_Core.getSkillStats(player).increaseSkill(Global.SKILL_PROSPECTING, isDeepSearch?10 : 1);
 		SkillRank rank = TFC_Core.getSkillStats(player).getSkillRank(Global.SKILL_PROSPECTING);
-
 		int index = random.nextInt(results.size());
 		ProspectResult result = results.values().toArray(new ProspectResult[0])[index];
 		String oreName = result.itemStack.getUnlocalizedName() + ".name";
@@ -298,15 +299,16 @@ public class ItemProPick extends ItemTerra
 			for (String str : AreaMsgs) AreaMsg.append(TFC_Core.l10n(str)).append(" ");
 		}
 		TFC_Core.sendInfoMessage(player,
-				new ChatComponentTranslation(quantityMsg)
+				new ChatComponentTranslation("")
+				.appendSibling(isDeepSearch?new ChatComponentTranslation("gui.ProPick.deepSearch"):new ChatComponentText(""))
+				.appendSibling(new ChatComponentTranslation(quantityMsg))
+				.appendSibling(new ChatComponentTranslation(oreName))
 				.appendText(" ")
-				.appendSibling(new ChatComponentTranslation(oreName)				.appendText(" ")
-				.appendText(" ")
-				.appendSibling(new ChatComponentTranslation(LocationMsg)
+				.appendSibling(new ChatComponentTranslation(LocationMsg))
 			    .appendText(" ")
-			    .appendSibling(new ChatComponentTranslation(DistanceMsg1)
-				.appendSibling(new ChatComponentText(DistanceMsg2)
-				.appendSibling(new ChatComponentText(AreaMsg.toString())))))));
+			    .appendSibling(new ChatComponentTranslation(DistanceMsg1))
+				.appendSibling(new ChatComponentText(DistanceMsg2))
+				.appendSibling(new ChatComponentText(AreaMsg.toString())));
 				
 		oreName = null;
 		result = null;
