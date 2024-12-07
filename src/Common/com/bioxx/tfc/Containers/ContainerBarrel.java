bf4f1f21dc58e53c0144640a6f847720cd3e236e
kuzuanpa <misaka16384@outlook.com>
2024-12-07 14:57:16 +0800

    fix barrel duplicate

--- src/Common/com/bioxx/tfc/Containers/ContainerBarrel.java
+++ src/Common/com/bioxx/tfc/Containers/ContainerBarrel.java
@@ -1,36 +1,22 @@
 package com.bioxx.tfc.Containers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import com.bioxx.tfc.Containers.Slots.SlotBarrel;
-import com.bioxx.tfc.Core.Player.PlayerTracker;
-import cpw.mods.fml.common.FMLLog;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.inventory.ICrafting;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.ChatStyle;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.world.World;
-
-import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.fluids.FluidStack;
-
 import com.bioxx.tfc.Containers.Slots.SlotChest;
 import com.bioxx.tfc.Containers.Slots.SlotForShowOnly;
 import com.bioxx.tfc.Core.Player.PlayerInventory;
 import com.bioxx.tfc.TileEntities.TEBarrel;
-import com.bioxx.tfc.api.TFCBlocks;
 import com.bioxx.tfc.api.Enums.EnumSize;
-import org.apache.logging.log4j.Level;
+import com.bioxx.tfc.api.TFCBlocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.ICrafting;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContainerBarrel extends ContainerTFC
 {
@@ -67,7 +53,7 @@ public class ContainerBarrel extends ContainerTFC
 		{
 			//Input slot
 			if(!barrel.getSealed())
-				addSlotToContainer(new SlotBarrel(barrel, 0, 80, 29).setSize(EnumSize.LARGE).addItemException(getExceptions()));
+				addSlotToContainer(new SlotChest(barrel, 0, 80, 29).setSize(EnumSize.LARGE).addItemException(getExceptions()));
 			else
 				addSlotToContainer(new SlotForShowOnly(barrel, 0, 80, 29));
 		}
@@ -78,7 +64,7 @@ public class ContainerBarrel extends ContainerTFC
 				for(int k = 0; k < 3; k++)
 				{
 					if(!barrel.getSealed())
-						addSlotToContainer(new SlotBarrel(barrel, k+(i*3), 53+(i*18), 17+(k*18)).setSize(EnumSize.LARGE).addItemException(ContainerChestTFC.getExceptions()));
+						addSlotToContainer(new SlotChest(barrel, k+(i*3), 53+(i*18), 17+(k*18)).setSize(EnumSize.LARGE).addItemException(ContainerChestTFC.getExceptions()));
 					else
 						addSlotToContainer(new SlotForShowOnly(barrel, k+(i*3), 53+(i*18), 17+(k*18)));
 				}
